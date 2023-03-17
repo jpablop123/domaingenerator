b19b440aeffd1fa68a817ee50aef4c03c3682579
jpablop123 <juanpablopinto123@icloud.com>
2023-03-17 18:07:43 +0000

    revision

--- src/app.js
+++ src/app.js
@@ -6,6 +6,40 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("hello world");
+  for (var noun = 0; noun < pronoun.length; noun++) {
+    for (var adjetivo = 0; adjetivo < adj.length; adjetivo++) {
+      for (var nombre = 0; nombre < nouns.length; nombre++) {
+        for (var extensiones = 0; extension < extension.length; extensiones++) {
+          console.log(
+            pronoun[noun] +
+              adj[adjetivo] +
+              nouns[nombre] +
+              extension[extensiones] +
+              "<br/>"
+          );
+        }
+      }
+    }
+  }
 };
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var nouns = ["jogger", "racoon"];
+var extension = [".com", ".net", ".ios", ".bos"];
+
+for (var noun = 0; noun < pronoun.length; noun++) {
+  for (var adjetivo = 0; adjetivo < adj.length; adjetivo++) {
+    for (var nombre = 0; nombre < nouns.length; nombre++) {
+      for (var extensiones = 0; extension < extension.length; extensiones++) {
+        console.log(
+          pronoun[noun] +
+            adj[adjetivo] +
+            nouns[nombre] +
+            extension[extensiones] +
+            "<br/>"
+        );
+      }
+    }
+  }
+}
